1cbab2b3024da3bf7b2194247d9d56417bfdb1ad
lexymcr <@maingard.alexa@gmail.com>
2021-12-03 16:36:57 +0100

    Basic Finished Exercise (remove and total)

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,43 +1,62 @@
 import "../styles/cart.css"
 
-// Hardcode of an item
 const CartItem = (props) => {
-    return (
-        <li>
-            <img
-                className="cart--item-icon"
-                src={`./assets/icons/${props.cartItem.item.id}.svg`}
-                alt={props.cartItem.name}
-            />
-            <p>{props.cartItem.item.name}</p>
-            <button className="quantity-btn remove-btn center">-</button>
-            <span className="quantity-text center">{props.cartItem.quantity}</span>
-            <button className="quantity-btn add-btn center">+</button>
-        </li>
-    )
+  return (
+      <li>
+          <img
+              className="cart--item-icon"
+              src={`./assets/icons/${props.cartItem.item.id}.svg`}
+              alt={props.cartItem.name}
+          />
+          <p>{props.cartItem.item.name}</p>
+          <button 
+            className="quantity-btn remove-btn center"
+            onClick={() => props.removeFromCart(props.cartItem)}  
+          >-</button>
+          <span className="quantity-text center">{props.cartItem.quantity}</span>
+          <button 
+            className="quantity-btn add-btn center"
+            onClick={() => props.addToCart(props.cartItem)}
+          >+</button>
+      </li>
+  )
 }
 
 const Cart = (props) => {
-  console.log(props.cartItems)
-    return (
-        <main id="cart">
-        <h2>Your Cart</h2>
-        <div className="cart--item-list-container">
-          <ul className="item-list cart--item-list">
-            {props.cartItems.map(cartItem =>
-            <CartItem cartItem={cartItem}/>)}
-          </ul>
+  const calculateTotal = () => {
+    let total = 0;
+    props.cartItems.map(cartItem => {
+      total += cartItem.quantity * cartItem.item.price;
+    });
+    return total.toFixed(2);
+  }
+
+  let total = '£' + calculateTotal();
+
+  return (
+      <main id="cart">
+      <h2>Your Cart</h2>
+      <div className="cart--item-list-container">
+        <ul className="item-list cart--item-list">
+          {props.cartItems.map(cartItem =>
+            <CartItem 
+              cartItem={cartItem}
+              addToCart={props.addToCart}
+              removeFromCart={props.removeFromCart}
+            />)
+          }
+        </ul>
+      </div>
+      <div className="total-section">
+        <div>
+          <h3>Total</h3>
         </div>
-        <div className="total-section">
-          <div>
-            <h3>Total</h3>
-          </div>
-          <div>
-            <span className="total-number">£0.00</span>
-          </div>
+        <div>
+          <span className="total-number">{total}</span>
         </div>
-      </main>
-    )
+      </div>
+    </main>
+  )
 }
 
 export default Cart
--- src/components/Store.js
+++ src/components/Store.js
@@ -6,111 +6,134 @@ import Cart from './Cart'
 import Footer from './Footer'
 
 const initialStoreItems = [
-    {
+  { 
     "id": "001-beetroot",
     "name": "beetroot",
     "price": 0.35
-    },
-    {
-    "id": "002-carrot",
-    "name": "carrot",
-    "price": 0.35
-    },
-    {
-    "id": "003-apple",
-    "name": "apple",
-    "price": 0.35
-    },
-    {
-    "id": "004-apricot",
-    "name": "apricot",
-    "price": 0.35
-    },
-    {
-    "id": "005-avocado",
-    "name": "avocado",
-    "price": 0.35
-    },
-    {
-    "id": "006-bananas",
-    "name": "bananas",
-    "price": 0.35
-    },
-    {
-    "id": "007-bell-pepper",
-    "name": "bell pepper",
-    "price": 0.35
-    },
-    {
-    "id": "008-berry",
-    "name": "berry",
-    "price": 0.35
-    },
-    {
-    "id": "009-blueberry",
-    "name": "blueberry",
-    "price": 0.35
-    },
-    {
-    "id": "010-eggplant",
-    "name": "eggplant",
-    "price": 0.35
-    }
+  },
+  {
+  "id": "002-carrot",
+  "name": "carrot",
+  "price": 0.35
+  },
+  {
+  "id": "003-apple",
+  "name": "apple",
+  "price": 0.35
+  },
+  {
+  "id": "004-apricot",
+  "name": "apricot",
+  "price": 0.35
+  },
+  {
+  "id": "005-avocado",
+  "name": "avocado",
+  "price": 0.35
+  },
+  {
+  "id": "006-bananas",
+  "name": "bananas",
+  "price": 0.35
+  },
+  {
+  "id": "007-bell-pepper",
+  "name": "bell pepper",
+  "price": 0.35
+  },
+  {
+  "id": "008-berry",
+  "name": "berry",
+  "price": 0.35
+  },
+  {
+  "id": "009-blueberry",
+  "name": "blueberry",
+  "price": 0.35
+  },
+  {
+  "id": "010-eggplant",
+  "name": "eggplant",
+  "price": 0.35
+  }
 ]
 
-
-
 const Store = () => {
-    const [storeItems, setStoreItems] = useState(initialStoreItems)
-    const [cartItems, setCartItems] = useState([])
+  const [storeItems, setStoreItems] = useState(initialStoreItems)
+  const [cartItems, setCartItems] = useState([])
 
-    const addToCart = (storeItem) => {
-        let alreadyInCart = false;
-        const updatedCartItems = cartItems.map(cartItem => {
-          if (storeItem.id === cartItem.item.id) {
-            const cartItemToUpdate = {
-              ...cartItem,
-              quantity: cartItem.quantity + 1
-            };
-            alreadyInCart = true;
-            return cartItemToUpdate;
-          } 
-          else {
-            return cartItem;
-          }
-        });
-        if (!alreadyInCart) {
-          const newCartItem = {
-            item: storeItem,
-            quantity: 1
+  const addToCart = storeItem => {
+    let alreadyInCart = false;
+    const updatedCartItems = cartItems.map(cartItem => {
+      if (storeItem.id === cartItem.item.id || (storeItem.item && storeItem.item.id === cartItem.item.id)) {
+        const cartItemToUpdate = {
+          ...cartItem,
+          quantity: cartItem.quantity + 1
+        };
+        alreadyInCart = true;
+        return cartItemToUpdate;
+      } 
+      else {
+        return cartItem;
+      }
+    });
+    if (!alreadyInCart) {
+      const newCartItem = {
+        item: storeItem,
+        quantity: 1
+      };
+      setCartItems([...cartItems, newCartItem]); 
+    } 
+    else {
+      setCartItems(updatedCartItems); 
+    }
+  }
+  
+  const removeFromCart = storeItem => {
+    const updatedCartItems = cartItems.map(cartItem => {
+      if(storeItem.item.id === cartItem.item.id){
+        if(cartItem.quantity > 1){
+          const cartItemToUpdate = {
+            ...cartItem,
+            quantity: cartItem.quantity - 1
           };
-          setCartItems([...cartItems, newCartItem]); 
-        } 
+          return cartItemToUpdate;
+        }
         else {
-          setCartItems(updatedCartItems); 
+          return null;
         }
       }
-    
+      else {
+        return cartItem;
+      }
+    });
+    const remainingCartItems = updatedCartItems.filter(cartItem => cartItem);
+    setCartItems(remainingCartItems);
+  }
 
-    return (
-        <>
-            <header id="store">
-            <h1>Greengrocers</h1>
-            <ul className="item-list store--item-list">
-            {storeItems.map(storeItem =>
-                <li>
-                <div className="store--item-icon">
-                    <img src={`./assets/icons/${storeItem.id}.svg`} alt={storeItem.name}/>
-                </div>
-                <button id={storeItem.name} onClick={() => addToCart(storeItem)}>Add to cart</button>
-                </li>
-                )}
-            </ul>
-        </header>
-        <Cart addToCart={addToCart} cartItems={cartItems}/>
-        <Footer /> 
-      </>
-    )
+  return (
+    <>
+      <header id="store">
+        <h1>Greengrocers</h1>
+        <ul className="item-list store--item-list">
+          {storeItems.map(storeItem =>
+              <li>
+              <div className="store--item-icon">
+                  <img src={`./assets/icons/${storeItem.id}.svg`} alt={storeItem.name}/>
+              </div>
+              <button id={storeItem.name} onClick={() => addToCart(storeItem)}>Add to cart</button>
+              </li>
+              )}
+        </ul>
+      </header>
+      <Cart 
+        addToCart={addToCart} 
+        cartItems={cartItems}
+        removeFromCart={removeFromCart}
+      />
+      <Footer /> 
+    </>
+  )
 }
 
 export default Store
